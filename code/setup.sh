d4587145553afed301cf9d74f8e7d5d7e03592bc
AlonsoUriarte14 <diego.santiago6@upr.edu>
2023-06-10 19:14:07 -0400

    setup.sh changes

--- code/setup.sh
+++ code/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 # enable i2c on le potato on pin 3 and 5
 ls -al /dev/i2c-*
 sudo ldto enable i2c-ao
 ls -al /dev/i2c-*
 
-pip3 install bme680
 
+pip3 install matplotlib
+
+git pull
 
